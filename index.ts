26b2bb9c0343543fb86eff469f4b809773e2d8dc
frekike22 <evelinjurinovics22@gmail.com>
2021-06-26 13:44:59 +0200

    2 files modified

--- index.ts
+++ index.ts
@@ -1,6 +1,38 @@
 // Import stylesheets
 import './style.css';
 
+
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+class toDo {
+  todo: Array<string> = [];
+  constructor(){
+  }
+  
+    add(todoText:string):Array<string>{
+      this.todo.push(todoText);
+      return this.todo;
+     }
+
+    list(){
+      
+      this.todo.forEach(function (value) {
+          document.createElement("li"+value);
+      });
+  }
+
+}
+const todo =  new toDo();
+
+todo.add("valami");
+todo.add("valamik");
+todo.list();
+
+const listDiv: HTMLElement = document.getElementById('list');
+
+  const input: HTMLElement = document.getElementById('input');
+
+const button: HTMLElement = document.getElementById('add');
+
+
+button.addEventListener("click", function(){todo.add(input.nodeValue)})
